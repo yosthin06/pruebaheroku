4b5ea7073edcf02661eac290f3bb6f2118ef98f6
Yosthin Galindo <yosthin.galindo@udem.edu>
2023-05-04 00:21:40 -0600

    camera

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, Response, request
+import cv2
+
 
 app = Flask(__name__)
 
-@app.route("/")
+def gen_frames():
+    
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+        
+            ret, buffer = cv2.imencode('.jpg', frame)
+            
+            frame = buffer.tobytes()
+            
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
     app.run()
